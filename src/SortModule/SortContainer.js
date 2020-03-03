022f7217e89bb0c1c99f5b8ff1bb95160d50b68a
Maciej Skwarczek <m.skwarczek@gmail.com>
2020-03-03 20:11:57 +0100

    Add selection sort algorithm visualization

--- src/SortModule/SortContainer.js
+++ src/SortModule/SortContainer.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import Toolbar from '../common/Toolbar';
 
 const SortContainer = () => {
 
   const [ data, updateData ] = useState([]);
-  const [ dataParams, setDataParams ] = useState({ size: 40, min: -100, max: 100 });
+  const [ dataParams, setDataParams ] = useState({ size: 30, min: -100, max: 100 });
+  const [ iteration, updateIteration ] = useState({ i: null, j: null });
+  const min = useRef(null);
 
   const generateData = () => {
     let newData = [];
@@ -15,31 +18,77 @@ const SortContainer = () => {
     updateData(newData);
   };
 
-  console.log(data);
+  useEffect(() => {
+    if ( iteration.i !== null) {
+      let intervalID = null;
+      if (iteration.i < data.length) {
+        intervalID = setTimeout(() => selectionSort(), 10);
+      } else {
+        clearTimeout(intervalID);
+      };
+      return () => clearTimeout(intervalID);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [iteration])
+
+  const selectionSort = () => {
+    if (!min.current) {
+      min.current = 0;
+    };
+    if (iteration.j < data.length) {
+      if (data[iteration.j] < data[min.current]) {
+        min.current = iteration.j;
+      };
+      return updateIteration(iteration => { return { i: iteration.i, j: iteration.j + 1 }})
+    };
+    if (min.current !== iteration.i) {
+      let newData = [ ...data ];
+      let temp = data[iteration.i];
+      newData[iteration.i] = data[min.current];
+      newData[min.current] = temp;
+      updateData(newData);
+    };
+    min.current = iteration.i + 1;
+    updateIteration(iteration => { return { i: iteration.i + 1, j: iteration.i + 2 }});
+  };
+
+  const resetChart = () => {
+    min.current = null;
+    updateIteration({ i: null, j: null });
+  };
 
   return (
     <div className='moduleContainer'>
       <Toolbar
         title='Sorting Algorithms'
       >
-        <p onClick={generateData}>Generate random data</p>
-        <p>Option 2</p>
-        <p>Option 3</p>
+        {iteration.i === null && <p onClick={generateData}>Generate random data</p>}
+        {iteration.i === null && data.length > 0 && <p onClick={() => updateIteration(() => { return { i: 0, j: 1 }})}>Selection sort</p>}
+        {iteration.i !== null && iteration.i === data.length && <p onClick={resetChart}>Reset chart</p>}
       </Toolbar>
       <div className='sortContainer'>
         <div className='sortContainer__chart'>
           {data.map((elem, index) => 
             <div
-              key={index}
+              key={uuidv4()}
               style={{
                 height: (elem - dataParams.min) / (dataParams.max - dataParams.min) * 100 + '%',
                 width: 80 / dataParams.size + '%',
                 margin: '1px',
-                backgroundColor: 'lightblue',
                 textAlign: 'center',
+                backgroundColor: 
+                  index === iteration.i
+                    ? 'red' 
+                    : index === iteration.j
+                      ? 'gray'
+                      : index === min.current
+                        ? 'blue'
+                        : index > iteration.i - 1
+                          ? 'lightgray'
+                          : 'lightblue',
               }}
             >
-              <p>{elem}</p>
+              <p className='sortContainer__label'>{elem}</p>
             </div>
           )}
         </div>
